13e4d3ae950b319b5fc85539967fffc8f00d800c
rohit goudagaon <rohitsgoudagaon@gmail.com>
2020-12-29 15:35:40 +0530

    emaillist in progess

--- src/EmailList.tsx
+++ src/EmailList.tsx
@@ -1,17 +1,39 @@
-import React from 'react';
-import './EmailList.css'
+import { Checkbox, IconButton } from "@material-ui/core";
+import React, { useState } from "react";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import RefreshIcon from "@material-ui/icons/Refresh";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
+import "./EmailList.css";
 interface IProps {
-    // name: string
+  // name: string
 }
 
 const EmailList = (props: IProps) => {
-    return (
-        <div className='emailist'>
-            Email List
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="emailist">
+      <div className="emaillist--tools">
+        <div className="emaillist--toolsLeft">
+          <div className="emaillist--toolsLeft-checkboxWrapper">
+            <Checkbox />
+            <IconButton
+              className="emaillist--toolsLeft-checkboxWrapper-arrow"
+              onClick={() => {
+                setIsOpen(!isOpen);
+              }}
+            >
+              <ArrowDropDownIcon />
+            </IconButton>
+          </div>
+
+          <RefreshIcon className="emaillist--toolsLeft-refresh" />
+          <MoreVertIcon className="emaillist--toolsLeft-more" />
         </div>
-    )
-}
+        <div className="emaillist--toolsRight">Right tools</div>
+      </div>
+    </div>
+  );
+};
 
-
-export default EmailList
-
+export default EmailList;
